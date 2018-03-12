ac7315ce03ae4a5e87d6448e892606a69cd2af7c
RaghavRaman <rghv.raman@gmail.com>
2018-03-12 14:44:32 -0400

    Profile page added

--- db.sql
+++ db.sql
@@ -23,3 +23,27 @@ CREATE TABLE IF NOT Exists `ruc_signup_token` (
    CONSTRAINT courses_PK PRIMARY KEY (`token`),
    CONSTRAINT `signup_token_FK_generated_by` FOREIGN KEY (`genreated_by`) REFERENCES `user_login`(`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT Exists `fact` (
+  `id` INTEGER NOT NULL,
+  `score_phrase` varchar(50),
+  `title` varchar(100),
+  `url` varchar(100),
+  `platform` varchar(50),
+  `score` FLOAT(2,1),
+  `genre` varchar(50),
+  `editors_choice` varchar(100),
+  `release_year` NUMBER(4),
+  `release_month` NUMBER(2),
+  `release_day` NUMBER(2),
+  PRIMARY KEY (id)
+);
+  score_phrase  title url platform  score genre editors_choice  release_year  release_month release_day
+  `genreated_by` int(11) NOT NULL,
+  `generated_for` varchar(100) NOT NULL,
+  `is_used` int(1) DEFAULT 0,
+  `created_at` datetime NOT NULL,
+   CONSTRAINT courses_PK PRIMARY KEY (`token`),
+   CONSTRAINT `signup_token_FK_generated_by` FOREIGN KEY (`genreated_by`) REFERENCES `user_login`(`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
